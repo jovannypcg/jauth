6e164e57e4b4786d21cc4eae42abd8a0f2b34956
Jovanny Cruz <jovannypcg@yahoo.com>
2017-09-10 16:53:54 -0500

    Add validations to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.new(name: "Jovanny", email: "jovannypcg@yahoo.com") }
+
+  it 'should be valid' do
+    is_expected.to be_valid
+  end
+
+  context "when name is not present" do
+    let(:user) { User.new(email: "blah@blah.com") }
+
+    it 'should be invalid' do
+      expect(user).to_not be_valid
+    end
+  end
 end
